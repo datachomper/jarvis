1d553a90b47776dafe0a1c8f8f7c78a2c1c7abd7
Russ Meyerriecks <datachomper@gmail.com>
2015-08-06 04:34:24 +0000

    jarvis.py: Cleaned up the source a bit before moving on

--- jarvis.py
+++ jarvis.py
@@ -9,6 +9,12 @@ import RPi.GPIO as GPIO
 
 button_pressed = False
 
+# For some reason the period size is stuck on this number
+PERIOD = 341
+FRAME_SIZE = 2
+CHUNK = PERIOD * FRAME_SIZE
+
+
 def decoder_init():
 	hmdir = '/usr/local/share/pocketsphinx/model/en-us/en-us'
 	dictd = '/home/pi/sphinx_generated_dic/jarvis_v2.dic'
@@ -36,8 +42,16 @@ def decode(decoder, buf):
 
 	return ret
 
-def get_audio(rx):
+def get_audio():
 	buf = ''
+
+	# Setup alsa recording interface for "default" sound card
+	rx = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, 'sysdefault')
+	rx.setchannels(1)
+	rx.setrate(16000)
+	rx.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+	rx.setperiodsize(341)
+
 	print "jarvis standing by ..."
 	GPIO.wait_for_edge(18, GPIO.FALLING)
 
@@ -51,15 +65,11 @@ def get_audio(rx):
 			buf += data
 		time.sleep(.001)
 
+	rx.close()
 	print "analyzing ..."
 	return buf
 
 if __name__ == '__main__':
-	# For some reason the period size is stuck on this number
-	PERIOD = 341
-	FRAME_SIZE = 2
-	CHUNK = PERIOD * FRAME_SIZE
-
 	# Setup button 18 for Push-to-talk
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -68,13 +78,8 @@ if __name__ == '__main__':
 	decoder = decoder_init()
 
 	while True:
-		# Setup alsa recording interface for "default" sound card
-	    	rx = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, 'sysdefault')
-		rx.setchannels(1)
-		rx.setrate(16000)
-		rx.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-		rx.setperiodsize(341)
-	
+		buf = get_audio()
+
 		# Setup alsa playback interface
 	    	tx = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL, 'sysdefault')
 		tx.setchannels(1)
@@ -82,10 +87,8 @@ if __name__ == '__main__':
 		tx.setformat(alsaaudio.PCM_FORMAT_S16_LE)
 		tx.setperiodsize(341)
 
-		buf = get_audio(rx)
 		for i in range(0, len(buf)-CHUNK, CHUNK):
 			tx.write(buf[i:i+CHUNK])
-			
-		print('Hypothesis', decode(decoder, buf))
-		rx.close()
 		tx.close()
+	
+		print('Hypothesis', decode(decoder, buf))
