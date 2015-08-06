8dd6bdf4161b55f8c9397f3a5ee0cf5a541b72ff
Russ Meyerriecks <datachomper@gmail.com>
2015-08-06 06:07:36 +0000

    jarvis.py: Fleshing out the command dispatcher

--- jarvis.py
+++ jarvis.py
@@ -2,10 +2,11 @@
 
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
-import thread
 import alsaaudio
 import time
 import RPi.GPIO as GPIO
+import random
+import wave
 
 button_pressed = False
 
@@ -81,7 +82,89 @@ def debug_voice_playback(buf):
 		tx.write(buf[i:i+CHUNK])
 	tx.close()
 
+def play(filename):
+	filename = "audio/" + filename + ".wav"
+	print "playing", filename
+
+	f = wave.open(filename, 'rb')
+
+	# Setup alsa playback interface
+	tx = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL, 'sysdefault')
+	tx.setchannels(f.getnchannels())
+	tx.setrate(f.getframerate())
+
+	# 8bit is unsigned in wav files
+	if f.getsampwidth() == 1:
+	    tx.setformat(alsaaudio.PCM_FORMAT_U8)
+	# Otherwise we assume signed data, little endian
+	elif f.getsampwidth() == 2:
+	    tx.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+	elif f.getsampwidth() == 3:
+	    tx.setformat(alsaaudio.PCM_FORMAT_S24_LE)
+	elif f.getsampwidth() == 4:
+	    tx.setformat(alsaaudio.PCM_FORMAT_S32_LE)
+	else:
+	    raise ValueError('Unsupported format')
+
+	tx.setperiodsize(PERIOD)
+
+	data = f.readframes(PERIOD)
+	while data:
+		tx.write(data)
+		data = f.readframes(PERIOD)
+
+	tx.close()
+	f.close()
+
+def unknown():
+	rand = random.randint(1,3)
+	play("repeat_" + str(rand))
+
+def confirm():
+	x = ['confirm_1', 'confirm_2', 'confirm_3', 'confirm_4', 'confirm_5',
+		'confirm_6', 'confirm_7', 'confirm_8', 'confirm_9',
+		'confirm_10', 'activated']
+	play(random.choice(x))
+
+def say_time():
+	clock = time.localtime()
+
+	rand = random.randint(1,2)
+	play("clock_time_" + rand)
+	play("num_" + clock.tm_hour)
+	play("num_" + clock.tm_min)
+	play("clock_oclock")
+	
+
+def action_tree(c):
+	# Unknown Request
+	if c == '':
+		unknown()
+
+	# Respond to just "jarvis"
+	elif c == 'JARVIS':
+		x = ['title_u_s', 'listening_on_2', 'listening_on_3', 'listening_on_4',
+			'listening_on_5', 'listening_on_6', 'listening_on_7']
+		play(random.choice(x))
+	
+	# Open faceplate
+	elif c.find('OPEN'):
+		confirm()
+
+	# Close faceplate
+	elif c.find('CLOSE'):
+		confirm()
+
+	elif c.find('TIME'):
+		say_time()
+
+	# Unknown Request
+	else:
+		unknown()
+
 if __name__ == '__main__':
+	random.seed(time.time())
+
 	# Setup button 18 for Push-to-talk
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -92,4 +175,7 @@ if __name__ == '__main__':
 	while True:
 		buf = get_audio()
 		#debug_voice_playback(buf)
-		print('Hypothesis', decode(decoder, buf))
+		command = decode(decoder, buf)
+		print('Hypothesis ', command)
+		action_tree(command)
+		
