28d7b0e6dd705e23d0c2cf8124a49920acf89620
Russ Meyerriecks <datachomper@gmail.com>
2015-08-11 03:32:10 +0000

    jarvis: updated sphinx knowledge base with a bunch of terms

--- jarvis.py
+++ jarvis.py
@@ -18,8 +18,8 @@ CHUNK = PERIOD * FRAME_SIZE
 
 def decoder_init():
 	hmdir = '/usr/local/share/pocketsphinx/model/en-us/en-us'
-	dictd = '/home/pi/sphinx_generated_dic/jarvis_v2.dic'
-	lmdir = '/home/pi/sphinx_generated_dic/jarvis_v2.lm'
+	dictd = '/home/pi/jarvis/sphinx_kb/jarvis_v3.dic'
+	lmdir = '/home/pi/jarvis/sphinx_kb/jarvis_v3.lm'
 
 	# Create a decoder with certain model
 	config = Decoder.default_config()
