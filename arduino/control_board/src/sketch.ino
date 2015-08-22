7d041029dd0c8d7d2aed097f5fc90e566f88b4ee
Russ Meyerriecks <datachomper@gmail.com>
2015-08-21 21:43:50 -0500

    arduino: Moving servos up and down for exwife

--- arduino/control_board/src/sketch.ino
+++ arduino/control_board/src/sketch.ino
@@ -7,12 +7,18 @@
 #define LED_BRIGHTNESS 4
 #define PARTY_MODE 5
 #define FACEPLATE_SERVO 6
+#define ROOF_SERVO 7
+#define CRADLE_SERVO 8
+#define FLAP_SERVO 9
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(16, 10, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(13, 10, NEO_GRB + NEO_KHZ800);
 #define INTERVAL 50
 #define STROBESPEED 250
 
-Servo helm_servo;
+Servo helm;
+Servo roof;
+Servo cradle;
+Servo flap;
 
 void setup()
 {
@@ -24,6 +30,19 @@ void setup()
 	}
 	pixels.show();
 
+	/* This seems to prevent strange crosstalk to servos that aren't
+ 	 * initialized yet */
+	helm.attach(9);
+	helm.detach();
+
+	roof.attach(11);
+	roof.detach();
+
+	cradle.attach(12);
+	cradle.detach();
+
+	flap.attach(13);
+	flap.detach();
 }
 
 void strobe() {
@@ -59,15 +78,17 @@ void loop()
 			}
 			pixels.show();
 
-			helm_servo.attach(9);
-			helm_servo.write(600);
+			helm.attach(9);
+			//TODO: should this be .writeMicroseconds()?
+			helm.write(600);
 			delay(1000);
-			helm_servo.detach();
+			helm.detach();
 		} else if (cmd == CLOSE_FACEPLATE) {
-			helm_servo.attach(9);
-			helm_servo.write(2100);
+			helm.attach(9);
+			//TODO: should this be .writeMicroseconds()?
+			helm.write(2100);
 			delay(1000);
-			helm_servo.detach();
+			helm.detach();
 			for (int i = 0; i < pixels.numPixels(); i++) {
 				pixels.setPixelColor(i, pixels.Color(126, 0, 0));
 			}
@@ -107,9 +128,39 @@ void loop()
 		} else if (cmd == FACEPLATE_SERVO) {
 			int pwm = Serial.parseInt();
 			if ((pwm > 0) && (pwm < 2500))
-				helm_servo.write(pwm);
-			Serial.print("moved to ");
-			Serial.println(pwm);
+				helm.attach(9);
+				helm.write(pwm);
+				Serial.print("faceplate moved to ");
+				Serial.println(pwm);
+				delay(1000);
+				helm.detach();
+		} else if (cmd == ROOF_SERVO) {
+			int pwm = Serial.parseInt();
+			if ((pwm > 0) && (pwm < 2500))
+				roof.attach(11);
+				roof.writeMicroseconds(pwm);
+				Serial.print("roof moved to ");
+				Serial.println(pwm);
+				delay(1000);
+				roof.detach();
+		} else if (cmd == CRADLE_SERVO) {
+			int pwm = Serial.parseInt();
+			if ((pwm > 0) && (pwm < 2500))
+				cradle.attach(12);
+				cradle.writeMicroseconds(pwm);
+				Serial.print("cradle moved to ");
+				Serial.println(pwm);
+				delay(1000);
+				cradle.detach();
+		} else if (cmd == FLAP_SERVO) {
+			int pwm = Serial.parseInt();
+			if ((pwm > 0) && (pwm < 2500))
+				flap.attach(13);
+				flap.writeMicroseconds(pwm);
+				Serial.print("flap moved to ");
+				Serial.println(pwm);
+				delay(1000);
+				flap.detach();
 		}
 	}
 
